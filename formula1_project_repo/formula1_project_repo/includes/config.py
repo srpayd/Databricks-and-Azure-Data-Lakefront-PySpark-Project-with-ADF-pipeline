b53e88f6b294abed28f972e19a0d8713a0d88f74
srpayd <srpayd>
2024-03-06 16:15:58 +0000

    azure-adf-pipeline

--- formula1_project_repo/formula1_project_repo/includes/config.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-raw_folder_path = '/mnt/formuladl101/raw'
-stage_folder_path = '/mnt/formuladl101/stage'
-presentation_folder_path = '/mnt/formuladl101/presentation'
-
-# COMMAND ----------
-
-
